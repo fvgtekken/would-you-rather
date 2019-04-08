5dff689cd350c97f6a6eea5e6ca68d7eae78f39c
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2019-04-08 15:20:12 -0300

    Import New Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
+import { Container, Row, Col } from 'reactstrap'
 import Navigation from './Navigation'
 import Login from './Login'
-import { Container, Row, Col } from 'reactstrap';
+import QuestionsContainer from './QuestionsContainer'
+import NewQuestion from './NewQuestion'
+import LeaderBoard from './LeaderBoard'
+import AwnserQuestion from './AwnserQuestion'
+import QuestionVotes from './QuestionVotes'
 
 import '../css/App.css';
 
 class App extends Component {
+
+
+ authedRoutes = () => (
+    <Switch>
+      <Route exact path='/' component={QuestionsContainer} />
+      <Route exact path='/awnserQuestion' component={AwnserQuestion} />
+      <Route exact path='/questionVotes' component={QuestionVotes} />
+      <Route exact path='/addQuestion' component={NewQuestion} />
+      <Route exact path='/leaderBoard' component={LeaderBoard} />
+      <Route exact path='/questions' component={QuestionsContainer} />
+      <Route exact path='/login' component={Login} />
+      <Redirect from='*' to='/' />
+    </Switch>
+  )
+
+
+
   render() {
     return (
-      <div className="App">
-      <Navigation />
-        <header className="App-header">
-           <Container>    
-                <Row>
-                  <Col sm={{ size: 4, order: 2, offset: 4 }}><Login /></Col>
-                </Row>
-           </Container>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Fragment>
+            <div className="App">
+            <Navigation />
+              <header className="App-header">
+                 <Container>    
+                      <Row>
+                        <Col sm={{ size: 8, order: 2, offset: 2 }}>
+                          {this.authedRoutes()}
+                        </Col>
+                      </Row>
+                 </Container>
+              </header>
+            </div>
+             </Fragment>
+         </BrowserRouter>
+       
     );
   }
 }
