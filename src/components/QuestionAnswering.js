0e7bd59a917dac5eacde74af36557bd01c3b5398
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2019-04-12 10:35:40 -0300

    Integrate QuestionAnswering compoennt

--- src/components/QuestionAnswering.js
+++ src/components/QuestionAnswering.js
@@ -1,38 +1,60 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleVote } from '../actions/shared'
 import { Row, Col, Button, FormGroup, Label, Input,  Card, 
-  CardHeader, CardBody, CardTitle, CardText } from 'reactstrap'
+  CardHeader, CardBody } from 'reactstrap'
 
 class QuestionAnswering extends Component {
 
   state = {
-    imgSrc: '/favicon.png',
-    userToSignIn: null,
-    disabled: false
-  }
-
-  handleChange = (e) => {
-    console.log(e);
+    selection: null,
+    disabled: true
   }
 
   handleSubmit = (e) => {
+    
     e.preventDefault()
-    console.log(e);
+    
+    const { dispatch, authedUser, question } = this.props
+    const answer = this.state.selection
+    
+    if (answer) {
+      dispatch(handleVote(authedUser, question.id, answer))
+    }
   }
 
-  renderForm = () => (
-    <form onSubmit={this.handleSubmit}>
+  handleChange = (e) => {
+    this.setState({
+      selection: e.target.value,
+      disabled: false
+    })
+  }
+
+
+  renderForm = () => {
+
+    const { question } = this.props
+    const { disabled } = this.state
+
+   return(<form onSubmit={this.handleSubmit}>
        <FormGroup tag="fieldset">
           <legend>Awnser A Question</legend>
           <FormGroup check>
             <Label check>
-              <Input type="radio" name="Awser" />{' '}
+              <Input type="radio" 
+                 onChange={this.handleChange}
+                name="opts"
+                value="optionOne" />{question.optionOne.text}
               Question 1
             </Label>
           </FormGroup>
           <FormGroup check>
             <Label check>
-              <Input type="radio" name="Awser" />{' '}
-               Question 1
+              <Input type="radio" 
+                onChange={this.handleChange}
+                name="opts"
+                value="optionTwo" />{question.optionTwo.text}
+               Question 2
             </Label>
           </FormGroup>
       </FormGroup>
@@ -41,33 +63,37 @@ class QuestionAnswering extends Component {
         type="submit" bsStyle="info">
         Enter
       </Button>
-    </form>
-  )
+    </form>)
+  }
 
 
 
 
 
   render() {
-    return (
-      <Row>
-        <Col sm="12" >
 
-        <Card style={{ color: '#333' }}>
-        <CardHeader>User1 Awnser</CardHeader>
-          <CardBody>
-            <CardText>
-            <CardTitle></CardTitle>
-              <CardText>{this.renderForm()}</CardText>         
-            </CardText>
-          </CardBody>
-        </Card>
-        </Col>
-      </Row>
-    );
-  }
+     const { author } = this.props
+
+        return (
+          <Row>
+           <Col sm="12" >
+            <Card style={{ color: '#333' }}>
+            <CardHeader> Asked by {author.name}</CardHeader>
+              <CardBody>
+              <img src={author.avatarURL} alt="..." className="w-100 h-100 rounded-top rounded-bottom" />        
+                  {this.renderForm()}      
+              </CardBody>
+            </Card>
+            </Col>
+          </Row>
+        );
+    }
 }
 
 
 
-export default QuestionAnswering
+function mapStateToProps ({ authedUser }) {
+  return { authedUser }
+}
+
+export default connect(mapStateToProps)(QuestionAnswering)
