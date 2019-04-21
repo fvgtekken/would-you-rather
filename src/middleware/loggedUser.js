8345cf14ee46be72f061b37cd47b17cac9bbabec
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2019-04-21 12:33:57 -0300

    Update logger middleware

---
+++ src/middleware/loggedUser.js
@@ -0,0 +1,66 @@
+import { SET_LOGGED_USER, GET_LOGGED_USER_FROM_LOCAL, LOGOUT_USER } from '../actions/loggedUser'
+
+
+
+
+const loggedUser = (store) => (next) => (action) => {
+
+   const LOCAL_NAME = 'loggedUser'
+   
+   switch (action.type) {
+
+    case SET_LOGGED_USER :
+ 
+      setUserLocal (LOCAL_NAME, action.idUser) 
+      return next(action)
+
+    case GET_LOGGED_USER_FROM_LOCAL :
+      
+      const authedUser = getUserLocal()
+      action.idUser = authedUser ? authedUser : null
+          
+       return next(action)
+
+    case LOGOUT_USER:
+      
+      removeUserLocal()
+      action.idUser = null;
+      return next(action)
+
+    default :
+      return next(action)
+  }
+}
+
+
+
+
+ function getUserLocal () {
+
+      if(localStorage.getItem('userLogged')) {
+         const ObjUser = JSON.parse(localStorage.getItem('userLogged'));
+         return ObjUser.idUser;
+      }
+  }
+
+
+  function setUserLocal (USER_NAME, idUser) {
+
+    if(!localStorage.getItem('userLogged')) {
+        localStorage.setItem('userLogged', JSON.stringify ({
+              user_name: USER_NAME,
+              idUser
+        }));
+    }
+  }  
+
+  function removeUserLocal () {
+
+      if(localStorage.getItem('userLogged')){
+          localStorage.removeItem('userLogged');  
+      }
+
+    
+  }
+
+export default loggedUser
