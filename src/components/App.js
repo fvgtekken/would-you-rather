d5d176db9a1cb5cc7053ad4bde67ba3f732b254a
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2019-05-01 19:42:15 -0300

    Update Router

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ import CreateQuestion from './typeQuestion/CreateQuestion'
 import LeaderBoard from './LeaderBoard'
 import { getAuthedUserFromLocal } from '../actions/loggedUser'
 import { fetchData } from '../actions/shared'
-
+import  PageNoFound  from './common/PageNoFound'
 
 
 
@@ -26,8 +26,8 @@ class App extends Component {
   loginRoutes = () => {
 
     return(<Switch>
-              <Route exact path='/login' component={Login} />
-              <Redirect from='*' to='/login' />
+              <Route exact path='/login' component={Login} />       
+              <Route component={ PageNoFound }/>
           </Switch>)
   }
 
@@ -35,49 +35,52 @@ class App extends Component {
  loggedRoutes = () => {
 
     return(<Switch>
-      <Route exact path='/home' component={Home} />
-      <Route exact path='/add' component={CreateQuestion} />
-      <Route exact path='/questions/:questionId' component={ QuestionsContainer } />
-      <Route exact path='/leaderBoard' component={LeaderBoard} />
-      <Redirect from='*' to='/home' />
-    
-    </Switch>);
+            <Route exact path='/home' component={Home} />
+            <Redirect from='/login' to='/home' />
+            <Route exact path='/add' component={CreateQuestion} />
+            <Route exact path='/questions/:questionId' component={ QuestionsContainer } />
+            <Route exact path='/leaderBoard' component={LeaderBoard} />
+            <Route component={ PageNoFound } />
+          </Switch>);
   }
 
 
 
   render() {
 
-      const {displayNav} = this.props
+    const {displayNav} = this.props
 
-    return (
-      <BrowserRouter>
-        <Fragment>
-          <LoadingBar className="loading" />
-            <div className="App">
-            <Navigation />
-              <header className="App-header">
-                 <Container>    
-                      <Row>
-                        <Col sm={{ size: 8, order: 2, offset: 2 }}>
-                          {displayNav
-                          ? this.loginRoutes()
-                          : this.loggedRoutes()}
-                        </Col>
-                      </Row>
-                 </Container>
-              </header>
-            </div>
-             </Fragment>
-         </BrowserRouter>
-    );
+  
+
+        return (
+          <BrowserRouter>
+            <Fragment>
+              <LoadingBar className="loading" />
+                <div className="App">
+                <Navigation />
+                  <header className="App-header">
+                     <Container>    
+                          <Row>
+                            <Col sm={{ size: 8, order: 2, offset: 2 }}>
+                              {displayNav
+                              ? this.loginRoutes()
+                              : this.loggedRoutes()}
+                            </Col>
+                          </Row>
+                     </Container>
+                  </header>
+                </div>
+                 </Fragment>
+             </BrowserRouter>
+        );
   }
 }
 
 function mapStateToProps ({ authedUser, questions }) {
   return {
     loading: Object.keys(questions).length === 0,
-    displayNav: authedUser === null
+    displayNav: authedUser === null,
+    authedUser
   }
 }
 
